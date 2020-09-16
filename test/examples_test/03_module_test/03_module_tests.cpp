1e0fbc051d78016f420fefa7477d95bf0fc14428
Hilario Villanueva <hilario.villanueva2@g.austincc.edu>
2020-09-16 17:55:57 -0500

    9/13

--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -1,7 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "if.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test is even function")
+{
+	REQUIRE(is_even(2) == true);
+	REQUIRE(is_even(3) == false);
+}
+
+TEST_CASE("Test overtime function")
+{
+	REQUIRE(overtime(20) == false);
+	REQUIRE(overtime(40) == false);
+	REQUIRE(overtime(41) == true);
+}
