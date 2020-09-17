41a7ada7697f018664ed014bc6440fe496084b4c
Hilario Villanueva <hilario.villanueva2@g.austincc.edu>
2020-09-17 03:22:55 -0500

    Homework 0

--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -13,7 +13,7 @@ int multiply_numbers (int num1)
 
 	return num1 * num2;
 }
-//homework 
+//homework 0
 
 
 
--- src/classwork/01_assign/types.h
+++ src/classwork/01_assign/types.h
@@ -4,4 +4,4 @@ int sum_numbers(int num1, int num2);
 
 int multiply_numbers(int num1);
 
-//homework 
+//homework 0
