df3f7145018d9b248b740773e943b65c886548fb
Hilario Villanueva <hilario.villanueva2@g.austincc.edu>
2020-09-17 02:38:56 -0500

    9/17 lesson

--- src/examples/04_module/01_while/main.cpp
+++ src/examples/04_module/01_while/main.cpp
@@ -1,7 +1,17 @@
 #include "while.h"
+#include <iostream>
+
+using std::cout; 	using std::cin;
 
 int main() 
 {
+	char choice = 'y';
 
+	while (choice == 'y' || choice == 'Y') 
+	{
+
+		cout<<"Enter y again to continue, any other character to exit. ";
+		cin>>choice;
+	}
 	return 0;
 }
--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "if.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -17,4 +18,14 @@ TEST_CASE("Test overtime function")
 	REQUIRE(overtime(20) == false);
 	REQUIRE(overtime(40) == false);
 	REQUIRE(overtime(41) == true);
+}
+
+TEST_CASE("Test switch menu function")
+{
+	REQUIRE(menu(0) == "Invalid Option");
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(5) == "Invalid Option");
 }
