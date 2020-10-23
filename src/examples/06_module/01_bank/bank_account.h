c6e37347aa3417a55451a0f167cceaa9322a6542
Ciberkid22 <ciberkid2290@gmail.com>
2020-10-23 17:31:10 -0500

    10/19

--- src/examples/06_module/01_bank/bank_account.h
+++ src/examples/06_module/01_bank/bank_account.h
@@ -3,10 +3,22 @@ class BankAccount
 {
 
     public://access specifier-makes class function/method avaiable to users of the class
+        BankAccount() : balance(0){} // user defined default constructor
+        BankAccount(int b) : balance(b){}//user created constructor
         int get_balance(){return balance;}//inline class function
         void deposit(int amount);
         void withdraw(int amount);
+        static int get_bank_balance(){return bank_balance}
 
     private://access modifier-hides variable from users of the class
         int balance;
-};//DON'T FORGET THE SEMICOLON!!!!!!!!!!!!! 
+    
+    //DON'T FORGET THE SEMICOLON!!!!!!!!!!!!! 
+
+
+    //THESE ARE NOT BANK ACCOUNT CLASSES! They are free classes
+    void display_bank_account(BankAccount& account);
+
+    BankAccount get_bank_account();
+
+};
--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -5,8 +5,15 @@ using std::cout;
 
 int main()
 {
-	BankAccount account;//create a variable of the class
+	//there's another class that retrieves the balance from a database
+
+	BankAccount account(100);//create a variable of the class
 	cout<<"Balance: "<<account.get_balance();//using a public class function
+
+	BankAccount new_account;
+	new_account.deposit(25);
+	cout<<"New Account Balance: "<<new_account.get_balance();//using a public class function
+
 
 
 	return 0;
