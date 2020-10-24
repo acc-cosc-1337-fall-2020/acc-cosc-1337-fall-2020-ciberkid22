76826adceb84191f21c4023e7962ef3a5655cc6d
Ciberkid22 <ciberkid2290@gmail.com>
2020-10-24 18:13:44 -0500

    bank

--- src/examples/06_module/01_bank/bank_account.cpp
+++ src/examples/06_module/01_bank/bank_account.cpp
@@ -1,18 +1,78 @@
 //bank_account.cpp
 #include "bank_account.h"
 
-void BankAccount::deposit(int amount)
+BankAccount::BankAccount(int b) : balance(b)
 {
-    if(amount > 0)
+    //code for validating data
+    bank_balance += balance;
+}
+
+void BankAccount::deposit(int amount)
+{   
+   if(amount > 0)
     {
         balance += amount;
+        bank_balance += amount;    
     }
 }
 
 void BankAccount::withdraw(int amount)
 {
-    if (amount > 0 && balance >= amount)
+    if(amount > 0 && balance >= amount)
     {
         balance -= amount;
+        bank_balance -= amount;    
     }
+}
+
+int BankAccount::bank_balance = 0;
+//FREE FUNCTION-FRIEND OF BANK ACCOUNT CLASS
+void display_balance(const BankAccount& a)
+{
+    std::cout<<"Friend display balance: "<<a.balance<<"\n";
+}
+
+std::ostream& operator<<(std::ostream& out, const BankAccount& a)
+{
+    out<<"Overload display balance: "<<a.balance<<"\n";
+
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, BankAccount& a)
+{
+    int amount;
+    std::cout<<"Enter amount: ";
+    in>>amount;
+    a.balance += amount;
+
+    return in;
+}
+
+BankAccount operator+(const BankAccount& a1, const BankAccount& a2)
+{
+    BankAccount account(a1.balance + a2.balance);
+
+    return account;
+}
+
+//FREE FUNCTIONS-DO NOT BELONG TO THE BANKACCOUNT CLASS
+void display_bank_account_data(BankAccount& b)
+{
+    b.deposit(50);
+    std::cout<<"Balance: "<<b.get_balance()<<"\n";    
+}
+
+BankAccount get_account()
+{
+    //Class that retrieves DB balance and creates BankAccount
+    BankAccount a(1000);
+    
+    return a;
+}
+
+void BranchBank::update_balance(int b)
+{
+    branch_balance += b;
+    BankAccount::bank_balance += b;
 }
--- src/examples/06_module/01_bank/bank_account.h
+++ src/examples/06_module/01_bank/bank_account.h
@@ -1,24 +1,51 @@
+#include<iostream>
+
 //bank_account.h
+#ifndef BRANCH_BANK_H
+#define BRANCH_BANK_H
+
+class BranchBank
+{
+public:
+    BranchBank(int b) : branch_balance(b){}
+    void update_balance(int b);
+    int get_branch_balance()const{return branch_balance;}
+
+private:
+    int branch_balance;
+};
+
+#endif
+
+
+#ifndef BANK_ACCOUNT_H//header guards
+#define BANK_ACCOUNT_H//header guards
+
 class BankAccount
 {
+public:
+    BankAccount() = default;//behave like the default constructor C++ creates
+    explicit BankAccount(int b);
+    int get_balance()const{return balance;}//inline function-const makes a function read only
+    void deposit(int amount);
+    void withdraw(int amount);
+    friend void display_balance(const BankAccount& a);//NOT A CLASS FUNCTION-it is a free function
+    friend void BranchBank::update_balance(int b);
+    static int get_bank_balance(){return bank_balance;}
+    friend std::ostream& operator<<(std::ostream& out, const BankAccount& a);
+    friend std::istream& operator>>(std::istream& in, BankAccount& a);
+    friend BankAccount operator+(const BankAccount& a1, const BankAccount& a2);
 
-    public://access specifier-makes class function/method avaiable to users of the class
-        BankAccount() : balance(0){} // user defined default constructor
-        BankAccount(int b) : balance(b){}//user created constructor
-        int get_balance(){return balance;}//inline class function
-        void deposit(int amount);
-        void withdraw(int amount);
-        static int get_bank_balance(){return bank_balance}
-
-    private://access modifier-hides variable from users of the class
-        int balance;
+protected:
+    int balance {0};//class member 
     
-    //DON'T FORGET THE SEMICOLON!!!!!!!!!!!!! 
+private://access specifier
+    static int bank_balance;
+    
+};//VERY IMPORTANT DON'T FORGET THE SEMICOLON!!!!!!!
 
+#endif//header guards
 
-    //THESE ARE NOT BANK ACCOUNT CLASSES! They are free classes
-    void display_bank_account(BankAccount& account);
-
-    BankAccount get_bank_account();
-
-};
+//THESE FUNCTIONS DON'T BELONG TO THE BANKACCOUNT CLASS
+void display_bank_account_data(BankAccount& b);//free function
+BankAccount get_account();//free function
