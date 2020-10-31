c4da48469db1efe8aa89bc19bc00aefc50807c0b
Ciberkid22 <ciberkid2290@gmail.com>
2020-10-30 23:16:20 -0500

    Homework 6 Completion

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -5,23 +5,31 @@ using std::string; using std::cout; using std::cin;
 
 int main() 
 {
+	char choice;
 	TicTacToe game;
 	string player1;
+	string winner;
 	int position;
 
-	cout<<"Welcome to Tic-Tac-Toe! Player 1, choose if you want to be 'X' or 'O' by entering their respective letters --> ";
-	cin>>player1;
+	do{
+		cout<<"Welcome to Tic-Tac-Toe! Player 1, choose if you want to be 'X' or 'O' by entering their respective letters --> ";
+		cin>>player1;
 
-	game.start_game(player1);
+		game.start_game(player1);
 
-	while(game.game_over() == false)
-	{
-		cout<<"Enter a number from 1 to 9 to select your position: ";
-		cin>>position;
+		while(game.game_over() == false)
+		{
+			cout<<"Enter a number from 1 to 9 to select your position: ";
+			cin>>position;
 
-		game.mark_board(position);
-		game.display_board();
-	}
-	cout<<"Game-Over";
+			game.mark_board(position);
+			game.display_board();
+		}
+		winner = game.get_winner();
+		cout<<"Game! The winner is: "<<winner<<"!\n";
+		cout<<"Would you like to play again? Enter 'y' to continue, any other key to exit.\n";
+		cin>>choice;
+		
+	}while(choice == 'y' || choice == 'Y');
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -13,12 +13,18 @@ class TicTacToe
         void mark_board(int position); //mark the board
         string get_player()const{return player;}; // return player value
         void display_board()const; //display the board in 3x3 format
+        string get_winner(){return winner;};
 
     private:
         void set_next_player(); //set player X or O
         bool check_board_full(); //check if board is full
         void clear_board(); //clear the board
+        bool check_column_win(); //check if won by column
+        bool check_row_win(); //check if won by row
+        bool check_diagnal_win(); //check if won by diagnol
+        void set_winner(); //set the winner
         string player;
+        string winner;
         vector<string> point{9, " "};
 
 };
